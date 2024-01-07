6c35848b5ab920eeacf1bf7ecdff60b67e0519a6
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-01-07 12:33:19 -0500

    changes made to cookie message

--- script.js
+++ script.js
@@ -29,3 +29,24 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const allSections = document.querySelectorAll('.section');
+const allButtons = document.getElementsByName('button');
+
+// creating and inserting elements
+
+const header = document.querySelector('.header');
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We are cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+header.append(message);
+
+// delete elements
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
